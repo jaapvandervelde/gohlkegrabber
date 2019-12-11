107b6b4a360bb5f3c89d83b956f86bbd8c6e8939
Jaap.VanDerVelde <jaap.vandervelde@bmtglobal.com>
2019-12-11 12:11:05 +1000

    configuration fixes, 0.2

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name='gohlkegrabber',
   packages=['gohlkegrabber'],
-  version='0.1',
+  version='0.2',
   license='MIT',
   description='Simple script to download .whl packages from www.lfd.uci.edu/~gohlke/pythonlibs.',
   author='BMT, Jaap van der Velde',
   author_email='jaap.vandervelde@bmtglobal.com',
   url='https://github.com/jaapvandervelde/gohlkegrabber',
-  download_url='https://github.com/jaapvandervelde/gohlkegrabber/archive/0.1.tar.gz',
+  download_url='https://github.com/jaapvandervelde/gohlkegrabber/archive/0.2.tar.gz',
   keywords=['package', 'download', 'gohlke', 'wheel'],
   install_requires=[
       'lxml>=4.4.2'
